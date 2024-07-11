5337c5a4634de0eeebdf9a54260da814fa16b4b2
Ashanti Hatchett <ahatchett02@gmail.com>
2024-07-11 00:45:23 +0000

    fixed bugs for multiplication

--- app.rb
+++ app.rb
@@ -36,6 +36,11 @@ get("/multiply") do
 end
 
 get("/wizard_multiply") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @result = @first_num * @second_num
+
   erb(:mult_result)
 end
 
